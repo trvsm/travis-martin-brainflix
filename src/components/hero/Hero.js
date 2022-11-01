4b48b632d2ef03dc790a128ba640d97b0c2e8543
travis <trvsmartin@gmail.com>
2022-10-31 21:17:04 -0700

    Hero placeholders
    <body>
    
    Issue #

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,8 +5,15 @@ const Hero = () => {
     // const videoBanner = {}
   return (
     <>
-      <video></video>
+      <video
+        controls
+        poster="https://i.kym-cdn.com/entries/icons/original/000/022/134/elmo.jpg"
+      ></video>
       <h1 className="video__title">video title</h1>
+      <div className="video__stats">Who Posted | Views | Timestamp | Likes</div>
+      <section className="video__description">
+        Description: Suspicio? Bene ... tunc ibimus? Quis uh ... CONEXUS locus
+      </section>
     </>
   );
 };
