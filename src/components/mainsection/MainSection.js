0be8f7531908759b934d9c0f573daf444698d5ab
travis <trvsmartin@gmail.com>
2022-10-31 21:44:11 -0700

    Comment section placeholders
    <body>
    
    Issue #

--- src/components/mainsection/MainSection.js
+++ src/components/mainsection/MainSection.js
@@ -1,10 +1,30 @@
-import './mainSection.scss';
+import "./mainSection.scss";
 
-const MainSection = () =>{
-    return(
-        <>
-        <div>Comments form and video specific comments</div></>
-    )
+const MainSection = () => {
+  return (
+    <section className="comments">
+      <div className="comments__form-flex">
+        <section className="comments__profile-pic"></section>
+        <form className="comments__form" id="conversation">
+          <label className="comments__label" for="comment">
+            JOIN THE CONVERSATION
+          </label>
+          <input
+            type="text"
+            className="comments__form-comment"
+            name="comment"
+            id="comment"
+            required
+            placeholder="Add a new comment"
+          />
+          <button className="comments__button" type="submit">
+            COMMENT
+          </button>
+        </form>
+      </div>
+      <div className="comments__messages"></div>
+    </section>
+  );
 };
 
 export default MainSection;
