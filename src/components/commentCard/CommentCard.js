4f546e7b054e8378f30699fadc74b4ed7b2e4b9c
travis <trvsmartin@gmail.com>
2022-11-01 06:37:38 -0700

    add flexbox styling to comment
    <body>
    
    Issue #
    1. not styled to style guide

--- src/components/commentCard/CommentCard.js
+++ src/components/commentCard/CommentCard.js
@@ -3,14 +3,16 @@ import "./commentCard.scss";
 const CommentCard = (props) => {
   return (
     <>
-      <div className="comment__profile-pic"></div>
-      <div className="comment__right-section">
-        <div className="comment__attribution">
-          <span className="comment__name">{props.user}</span>
-          <span className="comment__timestamp">{props.timestamp}</span>
+      <section className="comment">
+        <div className="comment__profile-pic"></div>
+        <div className="comment__right-section">
+          <div className="comment__attribution">
+            <span className="comment__name">{props.user}</span>
+            <span className="comment__timestamp">{props.timestamp}</span>
+          </div>
+          <p className="comment__comment">{props.comment}</p>
         </div>
-        <p className="comment__comment">{props.comment}</p>
-      </div>
+      </section>
     </>
   );
 };
