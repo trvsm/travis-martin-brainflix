f8973a71da4dd039a9ad5b91a4265cd6ce37d9b9
travis <trvsmartin@gmail.com>
2022-11-03 15:22:25 -0700

    style header
    <body>
    
    Issue #
    1. header icons
    2. cta button size

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,19 +6,21 @@ const Header = () => {
       <header className="header">
         <div className="header__logo">BrainFlix</div>
         <div className="header__right">
-          <form className="header__form" action="/">
-            <label htmlFor="search">
-              <input
-                className="header__search-input"
-                type="text"
-                id="search"
-                placeholder="search"
-              />
-            </label>
-          </form>
-          <div className="header__profile-pic header__profile-pic--small"></div>
-          <button className="header__button">Upload</button>
-          <div className="header__profile-pic header__profile-pic--med"></div>
+          <div className="header__search">
+            <form className="header__form" action="/">
+              <label className="header__label" htmlFor="search">
+                <input
+                  className="header__search-input"
+                  type="text"
+                  id="search"
+                  placeholder="search"
+                />
+              </label>
+            </form>
+            <div className="header__profile-pic header__profile-pic--small"></div>
+          </div>
+          <div className="header__upload"><button className="header__button">Upload</button>
+          <div className="header__profile-pic header__profile-pic--med"></div></div>
         </div>
       </header>
     </>
