5bd0273a86ae8f3662a87364f9551b7fabd2a885
travis <trvsmartin@gmail.com>
2022-11-06 10:10:33 -0800

    format timestamp to date
    <body>
    
    Issue #

--- src/components/mainsection/MainSection.jsx
+++ src/components/mainsection/MainSection.jsx
@@ -1,17 +1,36 @@
 import "./mainSection.scss";
 
+let dateString;
+const formatCommentDate = (dateMS) => {
+  //grabs current date as string
+  const inputDate = new Date(dateMS);
+  const stringDate = inputDate.toDateString();
+  const dateArr = stringDate.split(" ");
+  //splits date string to array
+  const trimDateArr = dateArr.slice(1, 4);
+  //trim date array to relevant fields
+  dateString = trimDateArr[0] + "/" + trimDateArr[1] + "/" + trimDateArr[2];
+  //converts array to '/' separated string
+  return dateString;
+  //returns current date
+};
+
 const MainSection = ({comments}) => {
 
   // a function to map from JSON to comment card
   return (
     comments.map((comment) => {
+      let time = () => {
+        formatCommentDate(comment.timestamp);
+      };
+      time();
       return (
             <section className="comment" key={comment.id}>        
         <div className="comment__profile-pic"></div>
         <div className="comment__right-section">
           <div className="comment__attribution">
             <span className="comment__name">{comment.name}</span>
-            <span className="comment__timestamp">{comment.timestamp}</span>
+            <span className="comment__timestamp">{dateString}</span>
           </div>
           <p className="comment__comment">{comment.comment}</p>
         </div>
