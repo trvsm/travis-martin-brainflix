245a0cf66ed1f127f55fc33fc4a7057e7dbcd108
travis <trvsmartin@gmail.com>
2022-11-07 09:23:43 -0800

    setup paths for upload page
    <body>
    
    Issue #

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import "./styles/main.scss";
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // source data
 import videoDetails from "../src/data/video-details.json";
-import videoData from "../src/data/videos.json";
 
 import Card from "./components/card/Card";
 import Form from "./components/form/Form";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import MainSection from "./components/mainsection/MainSection";
+import axios from "axios";
 
-// https://project-2-api.herokuapp.com/ BrainFlix api
+//info to make and store api requests
+const brainflixKey = "29ea6abf-4f80-41fe-996e-c95e8069ab12";
+const videoEndpoint = "https://project-2-api.herokuapp.com/videos?api_key=";
+const videoData = [];
+
+axios
+  .get(videoEndpoint + brainflixKey)
+  .then((response) => {
+    for (let index = 0; index < response.data.length; index++) {
+      videoData[index] = response.data[index];
+    }
+    return videoData;
+  })
+  //getting a string successfully; filter video list
+  .then((response) => {
+    console.log(typeof response[0].id);
+  });
 
 const nextVideoList = (videoId) => {
   return videoData.filter((video) => video.id !== videoId);
@@ -32,7 +49,7 @@ function App() {
 
   const clickEvent = (event, videoId) => {
     event.preventDefault();
-    console.log(event)
+    console.log(event);
     setDefaultVideo(videoId);
     setVideos(nextVideoList(videoId));
     setActiveDetails(getVideoDetails(videoId));
