b29d7afbee3882fe0c7b6f1fe2ed0d26038db946
travis <trvsmartin@gmail.com>
2022-11-10 14:55:57 -0800

    remove console logs, fix htmlFor error
    <body>
    
    Issue #
    styling details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles/main.scss";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams} from "react-router-dom";
 
 import Card from "./components/card/Card";
 import Form from "./components/form/Form";
@@ -20,60 +20,37 @@ const nextVideoList = (videoId) => {
 };
 
 function App() {
-  const [defaultVideo, setDefaultVideo] = useState(
-    ""
-  );
+  const [defaultVideo, setDefaultVideo] = useState("");
   // can change all defaultVideo to video details, except initial video shows in side
   const [videos, setVideos] = useState(videoData);
   const [activeDetails, setActiveDetails] = useState(videoDetails);
 
   const params = useParams();
-  const navigate = useNavigate();
 
-  // gets active video stats and comments, sets to state
-
-  // useEffect(() => {
-  //   axios
-  //     .get(`${videoEndpoint}/${defaultVideo}${brainflixKey}`)
-  //     .then((response) => {
-  //       videoDetails = response.data;
-  //       setActiveDetails(videoDetails);
-  //     });
-  // }, [defaultVideo]);
-
-  //gets video thumbnail, sets to next videos list, filters out video set to default (details)
-
-  // useEffect(() => {
-  //   axios.get(videoEndpoint + brainflixKey).then((response) => {
-  //     for (let index = 0; index < response.data.length; index++) {
-  //       videoData[index] = response.data[index];
-  //     }
-  //     setVideos(nextVideoList(defaultVideo));
-  //   });
-  // }, [defaultVideo]);
-
-  // when params updated make api call, use to set main video, & video details
-  // another API call to return to default??
 
   useEffect(() => {
     // I see that params is empty object on logo click, how to set state to default
-    console.log(params);
     if (params.videoId) {
       axios
         .get(`${videoEndpoint}/${params.videoId}${brainflixKey}`)
         .then((response) => {
-          let active = response.data
-          setDefaultVideo(response.data.id);
-          setActiveDetails(response.data);
-          //setVideos in separate call to get video list & filter
-          setVideos(active.filter((video) => video.id !== response.data.id));
+          let active = response.data;
+          setDefaultVideo(active.id);
+          setActiveDetails(active);
+          return active;
+        })
+        .then((active) => {
+          axios.get(`${videoEndpoint}${brainflixKey}`).then((response) => {
+            setVideos(response.data.filter((video) => video.id !== active.id));
+          });
         });
+      //setVideos in separate call to get video list & filter
+      // setVideos(active.filter((video) => video.id !== response.data.id));
     } else {
       axios
         .get(`${videoEndpoint}${brainflixKey}`)
         .then((response) => {
           const firstVid = response.data[0].id;
-          console.log(firstVid);
           setDefaultVideo(firstVid);
           setVideos(response.data.filter((video) => video.id !== firstVid));
           return firstVid;
